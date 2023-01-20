3c7e134fa731ee1c1c467099d3e9bc0bf58dabe3
Andrew MacIsaac <amacisaac95@gmail.com>
2023-01-19 23:47:34 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+function appendCat(name)
+{
+    const copyCat = [...cats, name];
+    return copyCat;
+}
+function prependCat(name)
+{
+    const copyCat = [name, ...cats];
+    return copyCat;
+}
+function removeLastCat()
+{
+   return cats.slice(0, cats.length - 1);   
+}
+function removeFirstCat()
+{
+    return cats.slice (1);
+}
